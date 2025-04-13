64e4dae6fff6ec1c752fcd086a3ace91918631ab
PritiSalvi1 <pritisalvi10603@gmail.com>
2025-04-13 18:40:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("DO Practical 5 Q2 | Hello, World!");
+        System.out.println("DO Practical 5 Q3 | Hello, World!");
     }
 }
